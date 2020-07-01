f892ef0b8e640486983b2f985d73c49d1d76a15b
theognis1002 <theognis1002@gmail.com>
2020-07-01 16:53:46 -0400

    updated secret key config.json

--- api/djangoapi/djangoapi/settings.py
+++ api/djangoapi/djangoapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-with open('config.json') as f:
+with open('djangoapi/config.json') as f:
     config = json.load(f)
+    
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config['SECRET_KEY']
 
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- api/djangoapi/main_api/models.py
+++ api/djangoapi/main_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Venue(models.Model):
     venue = models.CharField(max_length=255)
